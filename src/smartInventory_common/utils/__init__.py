4450e316ea8d0521bd857c0f1783dcfd70d29815
Baptiste <b.pellarin@uniandes.edu.co>
2022-07-28 16:22:29 -0500

    feature: custom pagination

--- src/smartInventory_common/utils/__init__.py
+++ src/smartInventory_common/utils/__init__.py
@@ -1,2 +1,4 @@
 from .equipments import BorrowType
 from .users import USER_ROLES
+from .inspection import InspectionType
+from .pagination import CustomPagination
---
+++ src/smartInventory_common/utils/pagination/custom_pagination.py
@@ -0,0 +1,13 @@
+from rest_framework.response import Response
+from rest_framework import pagination
+
+
+class CustomPagination(pagination.PageNumberPagination):
+    def get_paginated_response(self, data):
+        return Response({
+            'page_size': self.page_size,
+            'next_page': self.page.next_page_number() if self.page.has_next() else None,
+            'prev_page': self.page.previous_page_number() if self.page.has_previous() else None,
+            'count': self.page.paginator.count,
+            'results': data
+        })
