ad4a013f1e47e5fd8e445738f7bb44e2ac3bda5a
Baptiste <b.pellarin@uniandes.edu.co>
2022-08-02 15:12:00 -0500

    refactor: CouchDb attributes lib

--- src/smartInventory_common/management/commands/init_couchdb.py
+++ src/smartInventory_common/management/commands/init_couchdb.py
@@ -2,13 +2,15 @@ import django
 from django.conf import settings
 from django.core.management import BaseCommand
 
+from smartInventory_common.communication import CouchDB
+
 
 class InitCouchDB(BaseCommand):
     def handle(self, *args, **options):
         django.setup()
 
         """Create database"""
-        response, status_code = settings.COUCHDB_CONNECTION.request("PUT", "")
+        response, status_code = CouchDB(settings.COUCHDB).request("PUT", "")
 
         if status_code == 412:
             print("CouchDB already setup, exit...")
